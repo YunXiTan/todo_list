9a391ba593b33f8c0aa1a9854442abb4678da2ca
YunXiTan <tyunxi03@gmail.com>
2025-04-24 13:48:22 +0800

    v3

--- todo_list_app/lib/main.dart
+++ todo_list_app/lib/main.dart
@@ -31,9 +31,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 1;
   List<Task> todoList = [];
-  final TextEditingController _controller = TextEditingController();
-  int updateIndex = -1;
-  DateTime? selectedDate;
   DateTime focusedDay = DateTime.now();
   DateTime? selectedDay;
 
@@ -43,31 +40,9 @@ class _HomeScreenState extends State<HomeScreen> {
     selectedDay = focusedDay;
   }
 
-  void addTask(String task) {
+  void addTask(String task, DateTime? dueDate) {
     setState(() {
-      todoList.add(Task(title: task, dueDate: selectedDate));
-      _controller.clear();
-      selectedDate = null;
-    });
-  }
-
-  void updateTask(String task, int index) {
-    setState(() {
-      final oldTask = todoList[index];
-      todoList[index] = Task(
-        title: task,
-        dueDate: selectedDate,
-        isDone: oldTask.isDone,
-      );
-      updateIndex = -1;
-      _controller.clear();
-      selectedDate = null;
-    });
-  }
-
-  void deleteTask(int index) {
-    setState(() {
-      todoList.removeAt(index);
+      todoList.add(Task(title: task, dueDate: dueDate));
     });
   }
 
@@ -77,19 +52,79 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void pickDate(BuildContext context) async {
-    final DateTime? date = await showDatePicker(
-      context: context,
-      initialDate: selectedDate ?? DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
+  void deleteTask(int index) {
+    setState(() {
+      todoList.removeAt(index);
+    });
+  }
 
-    if (date != null) {
-      setState(() {
-        selectedDate = date;
-      });
-    }
+  void showAddTaskModal(BuildContext context) {
+    final TextEditingController taskController = TextEditingController();
+    DateTime? pickedDate = DateTime.now();
+
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+      ),
+      builder: (BuildContext ctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(ctx).viewInsets.bottom,
+            top: 20,
+            left: 20,
+            right: 20,
+          ),
+          child: StatefulBuilder(builder: (context, setState) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Add New Task",
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(height: 16),
+                TextField(
+                  controller: taskController,
+                  decoration: InputDecoration(
+                    labelText: "Task Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                CalendarDatePicker(
+                  initialDate: pickedDate!,
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                  onDateChanged: (date) {
+                    setState(() => pickedDate = date);
+                  },
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    if (taskController.text.trim().isNotEmpty) {
+                      addTask(taskController.text.trim(), pickedDate);
+                      Navigator.pop(ctx);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.indigo,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text("Add Task"),
+                ),
+                SizedBox(height: 20),
+              ],
+            );
+          }),
+        );
+      },
+    );
   }
 
   Widget buildCalendarPage() {
@@ -100,69 +135,68 @@ class _HomeScreenState extends State<HomeScreen> {
                 DateFormat('yyyy-MM-dd').format(selectedDay!))
         .toList();
 
-    return Column(
+    return Stack(
       children: [
-        TableCalendar(
-          focusedDay: focusedDay,
-          firstDay: DateTime.utc(2020, 1, 1),
-          lastDay: DateTime.utc(2030, 12, 31),
-          selectedDayPredicate: (day) => isSameDay(selectedDay, day),
-          onDaySelected: (selected, focused) {
-            setState(() {
-              selectedDay = selected;
-              focusedDay = focused;
-            });
-          },
-          calendarStyle: CalendarStyle(
-            todayDecoration: BoxDecoration(
-              color: Colors.indigo.shade300,
-              shape: BoxShape.circle,
-            ),
-            selectedDecoration: BoxDecoration(
-              color: Colors.indigo,
-              shape: BoxShape.circle,
-            ),
-            weekendTextStyle: TextStyle(color: Colors.red),
-            defaultTextStyle: TextStyle(color: Colors.black),
-          ),
-          headerStyle: HeaderStyle(
-            formatButtonVisible: false,
-            titleCentered: true,
-            titleTextStyle:
-                TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: filteredList.length,
-            itemBuilder: (context, index) {
-              final task = filteredList[index];
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      blurRadius: 10,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
+        Column(
+          children: [
+            TableCalendar(
+              focusedDay: focusedDay,
+              firstDay: DateTime.utc(2020, 1, 1),
+              lastDay: DateTime.utc(2030, 12, 31),
+              selectedDayPredicate: (day) => isSameDay(selectedDay, day),
+              onDaySelected: (selected, focused) {
+                setState(() {
+                  selectedDay = selected;
+                  focusedDay = focused;
+                });
+              },
+              calendarStyle: CalendarStyle(
+                todayDecoration: BoxDecoration(
+                  color: Colors.indigo.shade300,
+                  shape: BoxShape.circle,
                 ),
-                child: Row(
-                  children: [
-                    Checkbox(
-                      value: task.isDone,
-                      onChanged: (value) =>
-                          toggleDone(todoList.indexOf(task), value),
+                selectedDecoration: BoxDecoration(
+                  color: Colors.indigo,
+                  shape: BoxShape.circle,
+                ),
+                weekendTextStyle: TextStyle(color: Colors.red),
+                defaultTextStyle: TextStyle(color: Colors.black),
+              ),
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+                titleTextStyle:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredList.length,
+                itemBuilder: (context, index) {
+                  final task = filteredList[index];
+                  return Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          blurRadius: 10,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
+                    child: Row(
+                      children: [
+                        Checkbox(
+                          value: task.isDone,
+                          onChanged: (value) =>
+                              toggleDone(todoList.indexOf(task), value),
+                        ),
+                        Expanded(
+                          child: Text(
                             task.title,
                             style: TextStyle(
                               fontSize: 18,
@@ -172,77 +206,28 @@ class _HomeScreenState extends State<HomeScreen> {
                                   : null,
                             ),
                           ),
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Icon(Icons.access_time, size: 16, color: Colors.grey),
-                              SizedBox(width: 4),
-                              Text(
-                                task.dueDate != null
-                                    ? DateFormat('HH:mm').format(task.dueDate!)
-                                    : "--:--",
-                                style: TextStyle(color: Colors.grey[700]),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () => deleteTask(todoList.indexOf(task)),
+                        ),
+                      ],
                     ),
-                    IconButton(
-                      icon: Icon(Icons.edit, color: Colors.indigo),
-                      onPressed: () {
-                        setState(() {
-                          _controller.text = task.title;
-                          updateIndex = todoList.indexOf(task);
-                          selectedDate = task.dueDate;
-                        });
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.delete, color: Colors.red),
-                      onPressed: () => deleteTask(todoList.indexOf(task)),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    hintText: 'Enter task...',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(width: 8),
-              IconButton(
-                onPressed: () => pickDate(context),
-                icon: Icon(Icons.calendar_today, color: Colors.indigo),
-              ),
-              SizedBox(width: 8),
-              FloatingActionButton(
-                mini: true,
-                onPressed: () {
-                  if (_controller.text.trim().isEmpty) return;
-                  updateIndex != -1
-                      ? updateTask(_controller.text, updateIndex)
-                      : addTask(_controller.text);
+                  );
                 },
-                backgroundColor: Colors.indigo,
-                child: Icon(updateIndex != -1 ? Icons.edit : Icons.add),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: FloatingActionButton(
+            backgroundColor: Colors.indigo,
+            onPressed: () => showAddTaskModal(context),
+            child: Icon(Icons.add),
+          ),
+        )
       ],
     );
   }
@@ -255,7 +240,6 @@ class _HomeScreenState extends State<HomeScreen> {
         onToggleDone: toggleDone,
       ),
       buildCalendarPage(),
-      //Center(child: Text("Profile Screen Coming Soon")),
     ];
 
     return Scaffold(
@@ -264,8 +248,8 @@ class _HomeScreenState extends State<HomeScreen> {
         currentIndex: _selectedIndex,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.list), label: "Tasks"),
-          BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: "Calendar"),
-          //BottomNavigationBarItem(icon: Icon(Icons.person), label: "Mine"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month), label: "Calendar"),
         ],
         onTap: (index) {
           setState(() {
